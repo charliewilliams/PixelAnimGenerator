fb010570fd1713313da679d9d15a70239934931c
Charlie Williams <c@harl.ie>
2018-06-20 15:28:38 +0100

    Initial Commit

---
+++ PixelAnimGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixelAnimGenerator
+//
+//  Created by Charlie Williams on 20/06/2018.
+//  Copyright © 2018 Charlie Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PixelAnimGeneratorTests/PixelAnimGeneratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  PixelAnimGeneratorTests.swift
+//  PixelAnimGeneratorTests
+//
+//  Created by Charlie Williams on 20/06/2018.
+//  Copyright © 2018 Charlie Williams. All rights reserved.
+//
+
+import XCTest
+@testable import PixelAnimGenerator
+
+class PixelAnimGeneratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
